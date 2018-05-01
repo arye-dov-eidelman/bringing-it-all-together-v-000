9276d0c360719f17f7f716d101c1f16e1822709a
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-01 18:21:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
     binding.pry
     sql =  <<-SQL
       SELECT * FROM dogs
-      WHERE name = ? & breed = ?
+      WHERE name = ? AND breed = ?
     SQL
     row = DB[:conn].execute(sql, data[:name], data[:breed])[0]
     if row
