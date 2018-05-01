004976bc45858bb81519cdf9bf811cc6368df2dd
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-01 17:31:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,15 @@ class Dog
     @id = nil
     data.each{ |k, v| self.send("#{k}=", v) }
   end
+
+  def self.create_table
+    sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
