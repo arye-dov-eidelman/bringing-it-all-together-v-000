d35a3861c7e368a37a738af0ff821be4dbcdd0d9
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-01 17:20:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,9 @@ require 'pry'
 class Dog
   attr_accessor :name, :bread
 
-  def initialize(name:, bread: =nil)
+  def initialize(name:, bread: = nil)
     @name = name
     @bread = bread
   end
+
 end
